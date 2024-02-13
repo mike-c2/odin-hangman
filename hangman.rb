9b4828f7aeedcf2ed1f8ce75298f88ce1022ddea
Mike Costa <mcosta1@gmail.com>
2024-02-13 17:48:04 -0500

    Add hangman.rb file, containing the Hangman class
    The Hangman class is mostly just an empty shell right now.

---
+++ hangman.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+##
+# This class will be used to play the Hangman game
+class Hangman
+  DICTIONARY_FILE = 'google-10000-english-no-swears.txt'
+  MINIMUM_WORD_SIZE = 5
+  MAXIMUM_WORD_SIZE = 12
+end
